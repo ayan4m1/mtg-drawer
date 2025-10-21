0fc8cff32d961afa10bbaa75c1be97402e9d4cdc
ayan4m1 <andrew@bulletlogic.com>
2025-10-21 07:31:36 -0400

    feat: drag and drop for text deck file

--- src/pages/draw.tsx
+++ src/pages/draw.tsx
@@ -1,7 +1,7 @@
 import pluralize from 'pluralize';
 import { FormikErrors, useFormik } from 'formik';
 import { sampleSize, uniqueId } from 'lodash';
-import { Fragment, useRef, useState } from 'react';
+import { Fragment, useCallback, useRef, useState, DragEvent } from 'react';
 import {
   Accordion,
   Button,
@@ -29,89 +29,118 @@ export function Component() {
   const [activeKey, setActiveKey] = useState('deck');
   const [loading, setLoading] = useState(false);
   const [hands, setHands] = useState<DeckEntry[][]>([]);
-  const { errors, values, handleSubmit, handleChange } = useFormik({
-    initialValues: {
-      deck: '',
-      drawCount: 1
-    },
-    validate: ({ deck, drawCount }) => {
-      const result: FormikErrors<FormSchema> = {};
+  const [dragging, setDragging] = useState(false);
+  const { errors, values, handleSubmit, handleChange, setFieldValue } =
+    useFormik({
+      initialValues: {
+        deck: '',
+        drawCount: 1
+      },
+      validate: ({ deck, drawCount }) => {
+        const result: FormikErrors<FormSchema> = {};
 
-      if (!deck) {
-        result.deck = 'Deck cannot be empty!';
-      }
-
-      if (isNaN(drawCount) || drawCount < 1 || drawCount > 10000) {
-        result.drawCount = 'Draw count must be between 1 and 10k';
-      }
-
-      return result;
-    },
-    onSubmit: async ({ deck, drawCount }) => {
-      setLoading(true);
-      setActiveKey('');
-
-      const tempDeck: DeckEntry[] = [];
-      const cards = deck.split('\n').map((line) => line.trim().split(/\s+/));
-
-      for (const card of cards) {
-        const [rawCount, ...rest] = card;
-        const [set, ...names] = rest.reverse();
-        const count = parseInt(rawCount, 10);
-
-        if (isNaN(count)) {
-          continue;
+        if (!deck) {
+          result.deck = 'Deck cannot be empty!';
         }
 
-        const name = names.reverse().join(' ');
-        let cardInfo: CardInfoResponse = null;
-
-        if (cardInfoMap.current.has(`${name} ${set}`)) {
-          cardInfo = cardInfoMap.current.get(`${name} ${set}`);
-        } else {
-          try {
-            const cardInfoResponse = await fetch(
-              `https://api.scryfall.com/cards/named?exact=${encodeURIComponent(name)}&set=${encodeURIComponent(set.replace(/[()]/g, ''))}`
-            );
-            cardInfo =
-              (await cardInfoResponse.json()) as unknown as CardInfoResponse;
-
-            cardInfoMap.current.set(`${name} ${set}`, cardInfo);
-          } catch {
-            // no-op
-            console.error(`Failed to fetch card info for ${name}`);
-          }
-        }
-
-        const newCard = {
-          name,
-          set,
-          image: cardInfo?.image_uris?.png,
-          color: cardInfo?.color_identity?.join?.('') ?? '',
-          type: cardInfo?.type_line
-        };
-
-        for (let i = 0; i < count; i++) {
-          tempDeck.push({
-            ...newCard,
-            id: uniqueId()
-          });
-        }
-      }
-
-      setHands((hands) => {
-        const result = [...hands];
-
-        setLoading(false);
-
-        for (let i = 0; i < drawCount; i++) {
-          result.push(sampleSize(tempDeck, 7));
+        if (isNaN(drawCount) || drawCount < 1 || drawCount > 10000) {
+          result.drawCount = 'Draw count must be between 1 and 10k';
         }
 
         return result;
-      });
-    }
-  });
+      },
+      onSubmit: async ({ deck, drawCount }) => {
+        setLoading(true);
+        setActiveKey('');
+
+        const tempDeck: DeckEntry[] = [];
+        const cards = deck.split('\n').map((line) => line.trim().split(/\s+/));
+
+        for (const card of cards) {
+          const [rawCount, ...rest] = card;
+          const [set, ...names] = rest.reverse();
+          const count = parseInt(rawCount, 10);
+
+          if (isNaN(count)) {
+            continue;
+          }
+
+          const name = names.reverse().join(' ');
+          let cardInfo: CardInfoResponse = null;
+
+          if (cardInfoMap.current.has(`${name} ${set}`)) {
+            cardInfo = cardInfoMap.current.get(`${name} ${set}`);
+          } else {
+            try {
+              const cardInfoResponse = await fetch(
+                `https://api.scryfall.com/cards/named?exact=${encodeURIComponent(name)}&set=${encodeURIComponent(set.replace(/[()]/g, ''))}`
+              );
+              cardInfo =
+                (await cardInfoResponse.json()) as unknown as CardInfoResponse;
+
+              cardInfoMap.current.set(`${name} ${set}`, cardInfo);
+            } catch {
+              // no-op
+              console.error(`Failed to fetch card info for ${name}`);
+            }
+          }
+
+          const newCard = {
+            name,
+            set,
+            image: cardInfo?.image_uris?.png,
+            color: cardInfo?.color_identity?.join?.('') ?? '',
+            type: cardInfo?.type_line
+          };
+
+          for (let i = 0; i < count; i++) {
+            tempDeck.push({
+              ...newCard,
+              id: uniqueId()
+            });
+          }
+        }
+
+        setHands((hands) => {
+          const result = [...hands];
+
+          setLoading(false);
+
+          for (let i = 0; i < drawCount; i++) {
+            result.push(sampleSize(tempDeck, 7));
+          }
+
+          return result;
+        });
+      }
+    });
+  const handleDragEnter = useCallback(() => setDragging(true), []);
+  const handleDrop = useCallback(
+    (e: DragEvent) => {
+      e.preventDefault();
+      setDragging(false);
+
+      const { files } = e.dataTransfer;
+
+      if (files.length !== 1) {
+        return;
+      }
+
+      const [file] = files;
+
+      if (file.type.startsWith('text/')) {
+        const reader = new FileReader();
+
+        reader.onload = (readEvent) => {
+          setFieldValue('deck', readEvent.target.result);
+        };
+
+        reader.readAsText(file, 'utf-8');
+      }
+    },
+    [setFieldValue]
+  );
+  const handleDragLeave = useCallback(() => setDragging(false), []);
 
   return (
     <Fragment>
@@ -130,9 +159,14 @@ export function Component() {
                   <Form.Label>Paste Decklist w/ Set IDs</Form.Label>
                   <Form.Control
                     as="textarea"
+                    className={dragging ? 'border-success border-5' : null}
                     isInvalid={Boolean(errors.deck)}
                     name="deck"
                     onChange={handleChange}
+                    onDragEnter={handleDragEnter}
+                    onDragLeave={handleDragLeave}
+                    onDragOver={(e) => e.preventDefault()}
+                    onDrop={handleDrop}
                     rows={10}
                     value={values.deck}
                   />
